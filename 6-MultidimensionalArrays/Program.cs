247a9b4c6b2b58b40e8878c0fa046658ddc780a3
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-11 00:34:02 +0400

    testing commit

--- 6-MultidimensionalArrays/Program.cs
+++ 6-MultidimensionalArrays/Program.cs
@@ -8,6 +8,7 @@ namespace _6_MultidimensionalArrays
         {
             int[,] a = new int[3, 4] {
              {0, 1, 2, 3} ,   /*  initializers for row indexed by 0 */
+
              {4, 5, 6, 7} ,   /*  initializers for row indexed by 1 */
              {8, 9, 10, 11}   /*  initializers for row indexed by 2 */
                                         };
