c1852184ad7154b8fed01cfd2a5efa3fecd1ca6b
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-11 00:49:10 +0400

    test

---
+++ 1-Lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
