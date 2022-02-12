86251be8dc46e40ac30393ff0fdd63ef01c4156c
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-12 23:31:29 +0400

    adding info

--- 1-Lesson/Program.cs
+++ 1-Lesson/Program.cs
@@ -7,36 +7,27 @@ namespace _1_Lesson
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+            Console.ReadLine();
             Console.Read();
+            
             Console.ReadKey();
             Console.Write(true);
-            //Console.Out(8);
+            var number1 = Console.ReadLine();
+            var number2 = Console.ReadLine();
+            Console.WriteLine("number1+number2={0}",number1+number2);
 
+            int x = Console.Read();
+            Console.WriteLine(x);
 
+            Console.WriteLine("Input A=");
+            Console.Read();
+            Console.ReadLine();
+            Console.WriteLine("Input A=");
+            Console.Read();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.Write("Input tt=");
+            Console.WriteLine("A+B=");
 
         }
     }
