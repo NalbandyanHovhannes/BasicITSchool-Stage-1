5807331a3f129581a3b93d5876a0629f137d56fc
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-14 21:04:45 +0400

    Alice Test

--- 1-Lesson/Program.cs
+++ 1-Lesson/Program.cs
@@ -7,47 +7,15 @@ namespace _1_Lesson
         static void Main(string[] args)
         {
 
-            decimal u = 5;
-            var t = Math.Abs(u);
-            
-            Console.WriteLine();
-            //Console.WriteLine("Hellooooo");
+         
 
-            //Console.Write("WriteVS");
-
-            //Console.WriteLine("Writeline");
-            //Console.WriteLine("1");
-            //Console.ReadLine();
-            //Console.Read();
-
-            //Console.WriteLine("Hello World!");
-            //Console.Write("Hello World!");
-            //Console.ReadLine();
-            //Console.Read();
-           
-            Console.ReadKey();
-            
-           // Console.Write(true);
-
-            var name = Console.ReadLine();
-            Console.WriteLine("");
-            var number2 = Console.ReadLine();
-
-            Console.WriteLine("number1+number2={0}", name + number2);
-
-            int x = Console.Read();
-            Console.WriteLine(x);
-
-            if (Console.ReadKey().Key == ConsoleKey.Enter)
-            {
-                Console.WriteLine("Thanks for press Enter");
-            }
-
-            Console.WriteLine("Input A=");
+         
             Console.Read();
-            Console.ReadLine();
-            Console.WriteLine("Input A=");
-            Console.Read();
+
+
+
+
+
 
             Console.Write("Input tt=");
             Console.WriteLine("A+B=");
