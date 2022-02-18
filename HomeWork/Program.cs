cea157c5d99ff8b5153d24497cab2bd8287398ad
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-18 19:26:39 +0400

    today

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -6,20 +6,29 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            int number = Convert.ToInt32(Console.ReadLine());//2341
-            Console.WriteLine("---------");
+            //int number = Convert.ToInt32(Console.ReadLine());//2341
+            //Console.WriteLine("---------");
 
-            int lastNumber4 = number % 10;      //1
-            int newNumber3 = number / 10;       //234
-            int lastNumber3 = newNumber3 % 10;  //4
-            int newNumber2 = newNumber3 / 10;   //23
-            int lastNumber2 = newNumber2 % 10;  //3
-            int numNumber1 = newNumber2 / 10;   //2
-            int lastNumber1 = numNumber1 % 10;  //2
+            //int lastNumber4 = number % 10;      //1
+            //int newNumber3 = number / 10;       //234
+            //int lastNumber3 = newNumber3 % 10;  //4
+            //int newNumber2 = newNumber3 / 10;   //23
+            //int lastNumber2 = newNumber2 % 10;  //3
+            //int numNumber1 = newNumber2 / 10;   //2
+            //int lastNumber1 = numNumber1 % 10;  //2
 
-            int result = lastNumber1 + lastNumber2 + lastNumber3 + lastNumber4;
-            string text = "Result=" + result;
-            Console.WriteLine(text);
+            //int result = lastNumber1 + lastNumber2 + lastNumber3 + lastNumber4;
+            //string text = "Result=" + result;
+            //Console.WriteLine(text);
+
+            string str = "kaddkadkf";
+            var y=str.Substring(1);
+            //Console.WriteLine(y);
+            bool csse =y.Contains('d');
+
+            double d1 = 6.15d;
+            double d2 = 4.85d;
+          
         }
     }
 }
