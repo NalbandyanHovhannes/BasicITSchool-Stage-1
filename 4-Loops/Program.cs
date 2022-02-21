a66f04bdb3756b51c96b902c91709ab6816b1fc4
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-21 23:34:30 +0400

    String builder

--- 4-Loops/Program.cs
+++ 4-Loops/Program.cs
@@ -140,40 +140,7 @@ namespace _4_Loops
 
 
 
-            //1.StringBulder
-            // StringBuilder sb = new StringBuilder();
-
-            //or
-
-            // StringBuilder sb = new StringBuilder("its a stringbuilder");
-           // StringBuilder stringBuilder = new StringBuilder();
-
-            var sb = new StringBuilder("Название: ");
-            Console.WriteLine(sb);   // Название: 
-            Console.WriteLine($"Длина: {sb.Length}"); // 10
-            Console.WriteLine($"Емкость: {sb.Capacity}"); // 16
-
-            sb.Append(" Руководство");
-            Console.WriteLine(sb);   // Название: Руководство
-            Console.WriteLine($"Длина: {sb.Length}"); // 22
-            Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
-
-            sb.Append(" по C#");
-            Console.WriteLine(sb);   // Название: Руководство по C#
-            Console.WriteLine($"Длина: {sb.Length}"); // 28
-            Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
-
-
-            StringBuilder newsb = new StringBuilder();
-            StringBuilder sbb = new StringBuilder("Hello World!");
-
-            sbb =new StringBuilder ("Hello World!");
-
-            for (int intt = 0; intt < sbb.Length; intt++)
-
-                Console.Write(sbb[intt]);
-            string a = sbb.ToString();
-
+            
             //int j = 1;
             for (int i = 1, j = 1; i <=9; ++i)
             {
--- 7-StringBuilder/Program.cs
+++ 7-StringBuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _7_StringBuilder
 {
@@ -6,7 +7,40 @@ namespace _7_StringBuilder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1.StringBulder
+            // StringBuilder sb = new StringBuilder();
+
+            //or
+
+            // StringBuilder sb = new StringBuilder("its a stringbuilder");
+            // StringBuilder stringBuilder = new StringBuilder();
+
+            var sb = new StringBuilder("Название: ");
+            Console.WriteLine(sb);   // Название: 
+            Console.WriteLine($"Длина: {sb.Length}"); // 10
+            Console.WriteLine($"Емкость: {sb.Capacity}"); // 16
+
+            sb.Append(" Руководство");
+            Console.WriteLine(sb);   // Название: Руководство
+            Console.WriteLine($"Длина: {sb.Length}"); // 22
+            Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
+
+            sb.Append(" по C#");
+            Console.WriteLine(sb);   // Название: Руководство по C#
+            Console.WriteLine($"Длина: {sb.Length}"); // 28
+            Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
+
+
+            StringBuilder newsb = new StringBuilder();
+            StringBuilder sbb = new StringBuilder("Hello World!");
+
+            sbb = new StringBuilder("Hello World!");
+
+            for (int intt = 0; intt < sbb.Length; intt++)
+
+                Console.Write(sbb[intt]);
+            string a = sbb.ToString();
+
         }
     }
 }
