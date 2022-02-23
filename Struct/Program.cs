9b32f40dc110cd8b9bbb7c24beba48b87808ada6
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-02-24 00:26:19 +0400

    1

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -6,36 +6,23 @@ namespace Struct
     {
         static void Main(string[] args)
         {
-
             Coordinate point;
             point.x = 10;
             point.y = 20;
             Console.Write(point.x); //output: 10  
             Console.Write(point.y); //output: 20  
-
             //-----
-
             Coordinates points = new Coordinates(10, 20);
-
             Console.WriteLine(points.x); //output: 10  
             Console.WriteLine(points.y); //output: 20  
-
-
-            //--
-          
-        
-
+            //--    
             Console.WriteLine(point.x); //output: 0  
             Console.WriteLine(point.y); //output: 0  
-
-
-
         }
         struct Coordinate
         {
             public int x;
-            public int y;
-     
+            public int y;     
         }
 
 
@@ -55,7 +42,6 @@ namespace Struct
         {
             public int x { get; set; }
             public int y { get; set; }
-
             public  void SetOrigin()
             {
                 this.x = 0;
