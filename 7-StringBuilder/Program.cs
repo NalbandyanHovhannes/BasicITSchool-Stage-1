3d8118dbd6f4c85180296bbfc1890186a6f26fe1
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-07-06 21:27:55 +0400

    string lesson

--- 7-StringBuilder/Program.cs
+++ 7-StringBuilder/Program.cs
@@ -10,6 +10,109 @@ namespace _7_StringBuilder
 
 
 
+            //string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            
+            //Console.WriteLine("The length of the txt string is: " + txt.Contains('d'));
+            //txt.IndexOf('E');
+            //string u=txt.Replace('A', 'B');
+            //Console.WriteLine(txt);
+            //Console.WriteLine(txt);
+            //txt.Trim();
+
+            //string txtttt = "   ABCDEFGHI    JKLMNOP   QRSTUVWXYZ   ";
+            //Console.WriteLine(txtttt.Trim());
+            //Console.WriteLine(txtttt.TrimEnd());
+            //Console.WriteLine(txtttt.TrimStart());
+            //int y = 894551;
+            //string h = string.Concat(txt, txtttt);
+            //Console.WriteLine(h);
+
+            //Console.ReadKey();
+
+
+            //string firstName = "John ";
+            //string lastName = "Doe";
+            ////string name = string.Concat(firstName, lastName);
+            ////Console.WriteLine(name);
+
+            ////string firstName = "John";
+
+            ////string lastName = "Doe";
+            //string name = $"My full name is: {firstName}{lastName}";
+            //Console.WriteLine(name);
+
+
+            //string myString = "Hello";
+            //Console.WriteLine(myString[4]);  // Outputs "H"
+
+            //string mywString = "Hello";
+            //Console.WriteLine(mywString.IndexOf("e"));  // Outputs "1"
+
+
+
+
+
+
+            //STRINGBUILDERRRRR
+
+
+            //        using System.Text; // include at the top
+
+            //StringBuilder sb = new StringBuilder(); //string will be appended later
+            //                                        //or
+            //    StringBuilder sb = new StringBuilder("Hello World!");
+
+
+
+            //    StringBuilder sb = new StringBuilder(50); //string will be appended later
+            //                                              //or
+            //    StringBuilder sb = new StringBuilder("Hello World!", 50);
+
+
+            //    StringBuilder sb = new StringBuilder("Hello World!");
+
+            //for(int i = 0; i<sb.Length; i++)
+            //    Console.Write(sb[i]); // output: Hello World!
+
+
+            //StringBuilder sb = new StringBuilder("Hello World!");
+
+            //    var greet = sb.ToString(); //returns "Hello World!"
+
+
+            //    StringBuilder sb = new StringBuilder();
+            //    sb.Append("Hello ");
+            //sb.AppendLine("World!");
+            //sb.AppendLine("Hello C#");
+            //Console.WriteLine(sb);
+
+
+            //StringBuilder sb = new StringBuilder("Hello World!");
+            //    sb.Insert(5," C#"); 
+
+            //Console.WriteLine(sb); //output: Hello C# World!
+
+
+            //StringBuilder sb = new StringBuilder("Hello World!", 50);
+            //    sb.Remove(6, 7);
+
+            //Console.WriteLine(sb); //output: Hello
+
+            //StringBuilder sb = new StringBuilder("Hello World!");
+            //    sb.Replace("World", "C#");
+
+            //Console.WriteLine(sb);//output: Hello C#!
+
+            //var sb = new StringBuilder(32);
+
+
+
+
+
+
+
+
+
 
             //1.StringBulder
             // StringBuilder sb = new StringBuilder();
@@ -19,31 +122,31 @@ namespace _7_StringBuilder
             // StringBuilder sb = new StringBuilder("its a stringbuilder");
             // StringBuilder stringBuilder = new StringBuilder();
 
-            var sb = new StringBuilder("Название: ");
-            Console.WriteLine(sb);   // Название: 
-            Console.WriteLine($"Длина: {sb.Length}"); // 10
-            Console.WriteLine($"Емкость: {sb.Capacity}"); // 16
+            //var sb = new StringBuilder("Название: ");
+            //Console.WriteLine(sb);   // Название: 
+            //Console.WriteLine($"Длина: {sb.Length}"); // 10
+            //Console.WriteLine($"Емкость: {sb.Capacity}"); // 16
 
-            sb.Append(" Руководство");
-            Console.WriteLine(sb);   // Название: Руководство
-            Console.WriteLine($"Длина: {sb.Length}"); // 22
-            Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
+            //sb.Append(" Руководство");
+            //Console.WriteLine(sb);   // Название: Руководство
+            //Console.WriteLine($"Длина: {sb.Length}"); // 22
+            //Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
 
-            sb.Append(" по C#");
-            Console.WriteLine(sb);   // Название: Руководство по C#
-            Console.WriteLine($"Длина: {sb.Length}"); // 28
-            Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
+            //sb.Append(" по C#");
+            //Console.WriteLine(sb);   // Название: Руководство по C#
+            //Console.WriteLine($"Длина: {sb.Length}"); // 28
+            //Console.WriteLine($"Емкость: {sb.Capacity}"); // 32
 
 
-            StringBuilder newsb = new StringBuilder();
-            StringBuilder sbb = new StringBuilder("Hello World!");
+            //StringBuilder newsb = new StringBuilder();
+            //StringBuilder sbb = new StringBuilder("Hello World!");
 
-            sbb = new StringBuilder("Hello World!");
+            //sbb = new StringBuilder("Hello World!");
 
-            for (int intt = 0; intt < sbb.Length; intt++)
+            //for (int intt = 0; intt < sbb.Length; intt++)
 
-                Console.Write(sbb[intt]);
-            string a = sbb.ToString();
+            //    Console.Write(sbb[intt]);
+            //string a = sbb.ToString();
 
         }
     }
--- 7-StringBuilder/String.cs
+++ 7-StringBuilder/String.cs
@@ -8,25 +8,6 @@ namespace _7_StringBuilder
 {
     internal class String
     {
-            //string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            //Console.WriteLine("The length of the txt string is: " + txt.Length);
-            
-            
-            
-            //string firstName = "John ";
-            //string lastName = "Doe";
-            //string name = string.Concat(firstName, lastName);
-            //Console.WriteLine(name);
-            
-            //string firstName = "John";
-            //string lastName = "Doe";
-            //string name = $"My full name is: {firstName} {lastName}";
-            //Console.WriteLine(name);\
-            
-            //string myString = "Hello";
-            //Console.WriteLine(myString[0]);  // Outputs "H"
-            
-            //string myString = "Hello";
-            //Console.WriteLine(myString.IndexOf("e"));  // Outputs "1"
+        
     }
 }
